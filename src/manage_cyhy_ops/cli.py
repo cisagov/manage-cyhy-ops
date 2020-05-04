7e24df92ff0df537a33079768746c1db71812f32
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-05-03 20:13:05 -0400

    Implement the command line interface. Fix issues in the ManageOperators class after testing.

--- src/manage_cyhy_ops/cli.py
+++ src/manage_cyhy_ops/cli.py
@@ -1,24 +1,123 @@
 """Manage CyHy operator data SSM parameters.
 
 Usage:
-  manage-cyhy-ops add [--regions=REGIONS] [--overwrite] <SSH_KEY>...
-  manage-cyhy-ops list [--regions=REGIONS] <USERNAME>
-  manage-cyhy-ops remove [--regions=REGIONS] [--full] <USERNAME>
+  manage-cyhy-ops add [--debug] [--regions=REGIONS] [--overwrite] [--username=USERNAME] SSH_KEY
+  manage-cyhy-ops remove [--debug] [--regions=REGIONS] [--full] USERNAME
+  manage-cyhy-ops list [--debug] [--regions=REGIONS] USERNAME
   manage-cyhy-ops (-h | --help)
   manage-cyhy-ops --version
 
+Arguments:
+  SSH_KEY   An SSH key in the format "ssh-ed25519 <SSH key> <comment>".
+            Note: If a username is not provided with --username, then the
+            SSH key comment should be a username in the format "firstname.lastname".
+  USERNAME  A username in the format "firstname.lastname".
+
 Options:
-  -h --help    Show this message.
-  --version    Show version.
-  --regions    Comma delimited list of AWS regions to use
-               [default: "us-east-1,us-east-2,us-west-1,us-west2"].
-  --overwrite  Overwrite the user's SSH key if one already exists
-               [default: False].
-  --full       Also remove the user's SSH key from SSM [default: False].
+  -h --help          Show this message.
+  --version          Show version.
+  --debug            Enable debug messages.
+  --regions=REGIONS  Comma delimited list of AWS regions to use.
+                     [default: us-east-1,us-east-2,us-west-1,us-west-2]
+  --overwrite        Overwrite the user's SSH key if one already exists.
+  --full             Also remove the user's SSH key from SSM.
 """
+
+# Standard Python Libraries
+import logging
+import sys
+from typing import Any, Dict, List
+
+# Third-Party Libraries
+import docopt
+from schema import And, Or, Regex, Schema, SchemaError, Use
+
+from ._version import __version__
+from .ssm import ManageOperators
+
+ALLOWED_REGIONS = ["us-east-1", "us-east-2", "us-west-1", "us-west-2"]
+VALID_USERNAME = r"^[a-zA-Z0-9.\-_]*$"
+USERNAME_ERROR_MSG = (
+    'Username must be in the format "firstname.lastname", and can only consist '
+    'of letters, numbers, and the characters ".-_".'
+)
+USERNAME_VALIDATE = Or(
+    None,
+    And(
+        str,
+        Use(str.lower),
+        Regex(VALID_USERNAME),
+        lambda s: len(s.split(".")) >= 2,
+        error=f"USER {USERNAME_ERROR_MSG}",
+    ),
+)
 
 
 def main() -> int:
     """Provide an interface to manage CyHy operators."""
-    # do stuff.
+    args: Dict[str, str] = docopt.docopt(__doc__, version=__version__)
+
+    schema: Schema = Schema(
+        {
+            "--regions": And(
+                str,
+                lambda s: False
+                if False in map(lambda e: e in ALLOWED_REGIONS, s.split(","))
+                else True,
+                error=f"Invalid region(s) provided. Valid regions are: {ALLOWED_REGIONS}",
+            ),
+            "--username": USERNAME_VALIDATE,
+            "SSH_KEY": Or(
+                None,
+                And(
+                    str,
+                    lambda s: len(s.split(" ")) == 3,
+                    error="Invalid SSH key format.",
+                ),
+            ),
+            "USERNAME": USERNAME_VALIDATE,
+            str: object,  # No other validation to perform.
+        }
+    )
+
+    try:
+        validated_args: Dict[str, Any] = schema.validate(args)
+    except SchemaError as err:
+        # Exit because one or more of the arguments were invalid.
+        print(err, file=sys.stderr)
+        return 1
+
+    # Set up logging.
+    log_level: str = "DEBUG" if validated_args["--debug"] else "INFO"
+    logging.basicConfig(
+        format="%(asctime)-15s %(levelname)s %(message)s", level=log_level
+    )
+
+    logging.debug(validated_args)
+
+    try:
+        regions: List = validated_args["--regions"].split(",")
+        manager: ManageOperators = ManageOperators(regions)
+    except Exception as e:
+        logging.error(e)
+        return 1
+
+    if validated_args["add"]:
+        ssh_key: str = validated_args["SSH_KEY"]
+
+        if validated_args["--username"]:
+            username = validated_args["--username"]
+        else:
+            logging.debug("Using SSH key comment as username.")
+            try:
+                username = Schema(USERNAME_VALIDATE).validate(ssh_key.split(" ")[2])
+            except SchemaError as err:
+                logging.error(err)
+                return 1
+
+        manager.add_user(username, ssh_key, overwrite=validated_args["--overwrite"])
+    elif validated_args["remove"]:
+        manager.remove_user(validated_args["USERNAME"], validated_args["--full"])
+    elif validated_args["list"]:
+        manager.check_user(validated_args["USERNAME"])
     return 0
--- src/manage_cyhy_ops/ssm.py
+++ src/manage_cyhy_ops/ssm.py
@@ -26,9 +26,8 @@ class ManageOperators:
             try:
                 self.clients[region] = boto3.client("ssm", region_name=region)
             except ClientError as e:
-                logging.error(f"Unable to setup SSM client in region {region}:")
-                logging.error(e)
-                raise
+                logging.error(f"Unable to setup SSM client in region {region}.")
+                raise e
 
     def update_cyhy_ops_users(self, username: str, region: str, remove: bool = False):
         """Update the list of CyHy Operators to use when an instance is built."""
@@ -40,41 +39,64 @@ class ManageOperators:
                 Name=SSM_CYHY_OPS_USERNAMES, WithDecryption=True
             )
             users = response["Parameter"]["Value"].split(",")
-        except client.meta.client.exceptions.ParameterNotFound:
+        except client.exceptions.ParameterNotFound:
             logging.warning(
-                f'The CyHy Operators parameter "{SSM_CYHY_OPS_USERNAMES}"'
+                f'The CyHy Operators parameter "{SSM_CYHY_OPS_USERNAMES}" '
                 f'does not exist in region "{region}".'
             )
 
+        logging.debug("Current CyHy Operators: {users}.")
+
         if remove:
-            users.remove(username)
+            if username not in users:
+                logging.warning(
+                    f'User "{username}" is not in the list of active CyHy Operators.'
+                )
+            else:
+                users.remove(username)
         else:
-            users.append(username)
+            if username in users:
+                logging.warning(
+                    f'User "{username}" is already in the list of active '
+                    "CyHy Operators."
+                )
+            else:
+                users.append(username)
 
-        updated_users = ",".join(users)
+        if len(users) == 0:
+            try:
+                logging.warning(
+                    "No CyHy Operators left, deleting parameter "
+                    f'from region "{region}".'
+                )
+                # Response is an empty dictionary on success.
+                client.delete_parameter(Name=SSM_CYHY_OPS_USERNAMES)
+            except ClientError as e:
+                logging.error(e)
+        else:
+            updated_users = ",".join(users)
 
-        try:
-            # The SSM response on success currently only contains a version
-            # number and the parameter tier.
-            # Neither are useful to us at this time, so we don't store them..
-            client.put_parameter(
-                Name=SSM_CYHY_OPS_USERNAMES,
-                Value=updated_users,
-                Type="SecureString",
-                Overwrite=True,
-            )
-            logging.info(
-                f'Successfully added "{username}" to CyHy Operators in "{region}".'
-            )
-        except Exception as e:
-            logging.warning(
-                "Unable to update parameter"
-                f'"{SSM_CYHY_OPS_USERNAMES}"'
-                f'in region "{region}".'
-            )
-            logging.warning(e)
+            logging.debug(f'New CyHy Operators value: "{updated_users}".')
 
-    def add_user(self, username, ssh_key: str):
+            try:
+                # The SSM response on success currently only contains a version
+                # number and the parameter tier.
+                # Neither are useful to us at this time, so we don't store them..
+                client.put_parameter(
+                    Name=SSM_CYHY_OPS_USERNAMES,
+                    Value=updated_users,
+                    Type="SecureString",
+                    Overwrite=True,
+                )
+                logging.info(f'Successfully updated CyHy Operators in "{region}".')
+            except Exception as e:
+                logging.warning(
+                    f'Unable to update parameter "{SSM_CYHY_OPS_USERNAMES}" '
+                    f'in region "{region}".'
+                )
+                logging.warning(e)
+
+    def add_user(self, username: str, ssh_key: str, overwrite: bool = False):
         """Add an Operator to the Parameter Store."""
         # Should this be atomic?
         for region, client in self.clients.items():
@@ -82,22 +104,27 @@ class ManageOperators:
                 # The SSM response on success currently only contains a version
                 # number and the parameter tier.
                 # Neither are useful to us at this time, so we don't store them..
+                logging.debug(
+                    f'Adding SSH key to Parameter Store in "{region}" with key '
+                    f'"{SSM_SSH_KEY_PREFIX}{username}".'
+                )
                 client.put_parameter(
                     Name=f"{SSM_SSH_KEY_PREFIX}{username}",
                     Value=ssh_key,
                     Type="SecureString",
+                    Overwrite=overwrite,
                 )
                 logging.info(
-                    f'Successfully added "{username}"\'s SSH key to the'
+                    f'Successfully added "{username}"\'s SSH key to the '
                     f'Parameter Store in "{region}".'
                 )
-            except client.meta.client.exceptions.ParameterAlreadyExists:
+            except client.exceptions.ParameterAlreadyExists:
                 logging.warning(
-                    f'SSH key for "{username}" already exists in the'
+                    f'SSH key for "{username}" already exists in the '
                     f'Parameter Store for region "{region}".'
                 )
                 logging.warning(
-                    "If you need to overwrite this value, please use the"
+                    "If you need to overwrite this value, please use the "
                     '"--overwrite" switch.'
                 )
             except ClientError as e:
@@ -111,32 +138,37 @@ class ManageOperators:
         for region, client in self.clients.items():
             if full:
                 try:
+                    parameter_name = f"{SSM_SSH_KEY_PREFIX}{username}"
                     # Response is an empty dictionary on success.
-                    client.delete_parameter(
-                        Name=f"{SSM_SSH_KEY_PREFIX}{username}", WithDecryption=True
-                    )
-                except client.meta.client.exceptions.ParameterNotFound:
+                    client.delete_parameter(Name=parameter_name)
+                except client.exceptions.ParameterNotFound:
                     logging.warning(
-                        f'User "{username}" dot not have an SSH key stored in'
+                        f'User "{username}" dot not have an SSH key stored in '
                         f'the Parameter Store of region "{region}".'
                     )
                 except ClientError as e:
                     logging.error(e)
             self.update_cyhy_ops_users(username, region, remove=True)
+            logging.info(
+                f'Successfully removed "{username}" from CyHy Operators '
+                f'in region "{region}".'
+            )
 
     def check_user(self, username: str):
         """Check for the existence of an Operator and return information."""
         for region, client in self.clients.items():
             try:
-                response = client.get_parameter(Name=f"{SSM_SSH_KEY_PREFIX}{username}")
+                response = client.get_parameter(
+                    Name=f"{SSM_SSH_KEY_PREFIX}{username}", WithDecryption=True
+                )
                 logging.info(
-                    f'User "{username}" has the following SSH key in the'
+                    f'User "{username}" has the following SSH key in the '
                     f'Parameter Store of region "{region}":'
                 )
                 logging.info(response["Parameter"]["Value"])
-            except client.meta.client.exceptions.ParameterNotFound:
+            except client.exceptions.ParameterNotFound:
                 logging.info(
-                    f'User "{username}" does not have an SSH key in the'
+                    f'User "{username}" does not have an SSH key in the '
                     f'Parameter Store of region "{region}".'
                 )
             except ClientError as e:
@@ -151,11 +183,11 @@ class ManageOperators:
                 else:
                     user_status = "is not set"
                 logging.info(
-                    f'User "{username}" {user_status} as a CyHy Operator'
+                    f'User "{username}" {user_status} as a CyHy Operator '
                     f'in region "{region}".'
                 )
-            except client.meta.client.exceptions.ParameterNotFound:
+            except client.exceptions.ParameterNotFound:
                 logging.warning(
-                    f'The CyHy Operators parameter "{SSM_CYHY_OPS_USERNAMES}"'
+                    f'The CyHy Operators parameter "{SSM_CYHY_OPS_USERNAMES}" '
                     f'does not exist in region "{region}".'
                 )
