8fc4ed5df1d163789fe2335f98c195cae21c7455
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-12 14:26:11 -0400

    Prefer generator expressions to maps in two places
    This change gets rid of two C417 errors from our flake8 pre-commit
    linter.

--- src/manage_cyhy_ops/cli.py
+++ src/manage_cyhy_ops/cli.py
@@ -82,8 +82,7 @@ def main() -> None:
         {
             "--regions": And(
                 str,
-                lambda s: False
-                not in map(lambda r: r in ALLOWED_REGIONS, s.split(",")),
+                lambda s: False not in [r in ALLOWED_REGIONS for r in s.split(",")],
                 error=(
                     f"Invalid region(s) provided. Valid regions are: {ALLOWED_REGIONS}"
                 ),
@@ -163,5 +162,5 @@ def main() -> None:
 
     # Right now all return statuses from the Manager are 1, but that is not
     # guaranteed in the future. This handles any non-successful error code.
-    if True in map(lambda e: e != 0, results):
+    if True in [e != 0 for e in results]:
         sys.exit(1)
